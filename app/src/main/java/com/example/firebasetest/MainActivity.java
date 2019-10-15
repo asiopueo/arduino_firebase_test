b5c73dda1d20371f434aae7bca5d201b394a23c5
Martin <martin.lippl@pueo.de>
2019-10-15 19:54:14 +0200

    Added button functionality. Button and Text still missing.

--- app/src/main/java/com/example/firebasetest/MainActivity.java
+++ app/src/main/java/com/example/firebasetest/MainActivity.java
@@ -4,14 +4,63 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Add text here:
+
+
+        // Add buttons here:
+
     }
+
+
+    ledstatus1.addValueEventListener(new ValueEventListener() {
+        @Override
+        public void onDataChange(DataSnapshot dataSnapshot) {
+            // This method is called once with the initial value and again
+            // whenever data at this location is updated.
+            String value = dataSnapshot.getValue(String.class);
+            Log.d("file", "Value is: " + value);
+            textView1.setText(value);
+
+        }
+
+        @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("file", "Failed to read value.", error.toException());
+            }
+        });
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ledstatus1.setValue("ON");
+            }
+        });
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ledstatus1.setValue("OFF");
+            }
+
+    });
+
+
 }
